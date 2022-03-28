064b2f0e3a049afa97da9bbcd0519acfacbffd11
Peter Fischer <spotted@gmail.com>
2022-03-28 19:37:48 -0400

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Peter Fischer on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
